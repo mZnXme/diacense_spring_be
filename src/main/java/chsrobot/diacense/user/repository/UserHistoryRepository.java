da2b67bc8659998a8e920ccf62cd391a54350b30
สิริณัฏฐ์ ปาลวิสุทธิ์ <138756628+mZnXme@users.noreply.github.com>
2025-07-08 21:55:02 +0700

    feat: create UserHistoryRepository with get history list by day method

--- src/main/java/chsrobot/diacense/user/repository/UserHistoryRepository.java
+++ src/main/java/chsrobot/diacense/user/repository/UserHistoryRepository.java
@@ -1,12 +1,17 @@
 package chsrobot.diacense.user.repository;
 
+import chsrobot.diacense.user.model.User;
 import chsrobot.diacense.user.model.UserHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserHistoryRepository extends JpaRepository<UserHistory, Long> {
     Optional<UserHistory> findByUserId(Long userId);
+
+    List<UserHistory> findByUserAndCreatedAtAfter(User user, LocalDateTime dateTime);
 }
