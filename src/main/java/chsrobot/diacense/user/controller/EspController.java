cfb8ad2ed35b2a6d1cb919ce1160979ed1bc8c4e
สิริณัฏฐ์ ปาลวิสุทธิ์ <138756628+mZnXme@users.noreply.github.com>
2025-07-31 19:30:11 +0700

    feat: add validation for EspController

--- src/main/java/chsrobot/diacense/user/controller/EspController.java
+++ src/main/java/chsrobot/diacense/user/controller/EspController.java
@@ -2,6 +2,7 @@ package chsrobot.diacense.user.controller;
 
 import chsrobot.diacense.user.EspHistoryDto;
 import chsrobot.diacense.user.UserHistoryDto;
+import chsrobot.diacense.user.model.HistoryType;
 import chsrobot.diacense.user.service.UserHistoryService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,6 +22,22 @@ public class EspController {
 
     @PostMapping("/add")
     public ResponseEntity<Map<String, String>> addUserHistory(@RequestBody EspHistoryDto espHistoryDto) {
+        if (espHistoryDto.getUsername() == null && espHistoryDto.getType() == null && espHistoryDto.getValue() == 0) {
+            Map<String, String> response = new HashMap<>();
+            response.put("message", "Invalid input data");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+        }
+        if (espHistoryDto.getValue() < 0) {
+            Map<String, String> response = new HashMap<>();
+            response.put("message", "Value must be non-negative");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+        }
+        assert espHistoryDto.getType() != null;
+        if (!espHistoryDto.getType().equals(HistoryType.BLOOD_GLUCOSE) && !espHistoryDto.getType().equals(HistoryType.BREATH_ACETONE)) {
+            Map<String, String> response = new HashMap<>();
+            response.put("message", "Invalid type");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+        }
         try {
             userHistoryService.addNewHistoryByEsp(espHistoryDto);
             Map<String, String> response = new HashMap<>();
